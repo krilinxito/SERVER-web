be288cac00c1d332fad1abcbbebb4861dc74b9bb
krilinxito <barryallen4207@gmail.com>
2025-05-26 16:34:28 -0400

    actualizando captcha

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -8,15 +8,15 @@ const {
 } = require('../controllers/dashboard.controller');
 
 // Middleware de autenticación y autorización si lo tenés implementado
-// const { verificarToken, soloAdmin } = require('../middlewares/auth.middleware');
+const { verificarToken, soloAdmin } = require('../middlewares/auth.middleware');
 
 // Estadísticas semanales
-router.get('/ingresos-semanales', /*verificarToken, soloAdmin,*/ ingresosSemanalesController);
+router.get('/ingresos-semanales', verificarToken, soloAdmin, ingresosSemanalesController);
 
 // Ingresos por método (efectivo, tarjeta, etc.)
-router.get('/ingresos-metodos', /*verificarToken, soloAdmin,*/ ingresosPorMetodoController);
+router.get('/ingresos-metodos', verificarToken, soloAdmin, ingresosPorMetodoController);
 
 // Productos más vendidos
-router.get('/productos-mas-vendidos', /*verificarToken, soloAdmin,*/ productosMasVendidosController);
+router.get('/productos-mas-vendidos', verificarToken, soloAdmin, productosMasVendidosController);
 
 module.exports = router;
