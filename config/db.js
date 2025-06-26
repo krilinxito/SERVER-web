a1b1ec27b4060a9bb82eb9fa517156b6c3458cf4
krilinxito <barryallen4207@gmail.com>
2025-06-25 21:51:10 -0400

    cosas

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const fs = require('fs');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -7,9 +6,10 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync('./ca.pem') // El archivo .pem que te da Aiven
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-
-module.exports = pool;
